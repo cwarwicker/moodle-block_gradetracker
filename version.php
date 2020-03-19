72ba96a78e0ac4d79147f14aaa84e4e53872d1d8
Conn Warwicker <conn@cmrwarwicker.com>
2020-03-19 13:42:23 +0000

    Finalising codebase for release.

--- version.php
+++ version.php
@@ -1,11 +1,11 @@
 <?php
 // Moodle versions - This need to be updated when we want to force it to run the db/update.php script
 
-$plugin->version = 2020022700;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2020031900;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2017111300; // Moodle 3.4
 $plugin->cron = 3600;
 $plugin->component = 'block_gradetracker';
 $plugin->dependencies = array(
-    'block_bc_dashboard' => 2018070600,
-    'local_df_hub' => 2019071900
+    'block_bc_dashboard' => 2020031900,
+    'local_df_hub' => 2020031900
 );
