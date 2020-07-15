26ab113a2527fa867edbaeed718d36749811eb37
Conn Warwicker <conn@cmrwarwicker.com>
2020-07-15 20:28:29 +0100

    FIX-UP: /classes/df_hub directory
    This fixes most moodle coding standards errors & warnings.
    
    The following standards have NOT been checked against, so for ease of review you may wish to disable these from your codechecker:
    - moodle.Files.LineLength.TooLong
    - moodle.NamingConventions.ValidVariableName
    - moodle.NamingConventions.ValidFunctionName
    - moodle.Commenting.InlineComment.InvalidEndChar

--- classes/df_hub/stats.php
+++ classes/df_hub/stats.php
@@ -1,8 +1,29 @@
 <?php
-
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * This file is included in the df_hub plugin, to display stats about the gradetracker's usage.
+ *
+ * @copyright 2020 Conn Warwicker
+ * @package block_gradetracker
+ * @version 2.0
+ * @author Conn Warwicker <conn@cmrwarwicker.com>
+ */
 defined('MOODLE_INTERNAL') || die();
 
-require_once $CFG->dirroot . '/blocks/gradetracker/lib.php';
+require_once($CFG->dirroot . '/blocks/gradetracker/lib.php');
 
 // Count Structures
 $stats['structures'] = count( \GT\QualificationStructure::getAllStructures() );
