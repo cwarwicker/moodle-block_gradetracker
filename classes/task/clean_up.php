a6823d91e5183268de37ebfdb30f2764eaa4b5c0
Conn Warwicker <conn@cmrwarwicker.com>
2020-07-16 22:22:22 +0100

    FIX-UP: /classes/task directory
    This fixes most moodle coding standards errors & warnings.
    
    The following standards have NOT been checked against, so for ease of review you may wish to disable these from your codechecker:
    - moodle.Files.LineLength.TooLong
    - moodle.NamingConventions.ValidVariableName
    - moodle.NamingConventions.ValidFunctionName
    - moodle.Commenting.InlineComment.InvalidEndChar

--- classes/task/clean_up.php
+++ classes/task/clean_up.php
@@ -1,82 +1,79 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * This class defines the scheduled task to do clean up procedures
- * 
- * @copyright 2015 Bedford College
- * @package Bedford College Grade Tracker
- * @version 1.0
- * @author Conn Warwicker <cwarwicker@bedford.ac.uk> <conn@cmrwarwicker.com> <moodlesupport@bedford.ac.uk>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
+ * @copyright   2011-2017 Bedford College, 2017 onwards Conn Warwicker
+ * @package     block_gradetracker
+ * @version     2.0
+ * @author      Conn Warwicker <conn@cmrwarwicker.com>
  */
 
 namespace block_gradetracker\task;
 
-require_once $CFG->dirroot . '/blocks/gradetracker/lib.php';
+defined('MOODLE_INTERNAL') or die();
 
-class clean_up extends \core\task\scheduled_task
-{
-        
+require_once($CFG->dirroot . '/blocks/gradetracker/lib.php');
+
+class clean_up extends \core\task\scheduled_task {
+
     /**
      * Get the name of the task
      * @return type
      */
-    public function get_name(){
+    public function get_name() {
         return get_string('task:clean_up', 'block_gradetracker');
     }
-    
+
     /**
      * Execute the clean up
      * @global type $DB
      */
     public function execute() {
-        
+
         global $DB;
-                
+
         $GT = new \GT\GradeTracker();
-        
+
         // Clean up logs from the db
         $keepFor = $GT->getSetting('keep_logs_for');
-        if (is_int($keepFor) && $keepFor > 0)
-        {
-            
+        if (is_int($keepFor) && $keepFor > 0) {
+
             $ago = strtotime("-{$keepFor} days");
             mtrace("Deleting all logs created before " . date('d-m-Y, H:i', $ago));
 
             // Delete all logs and their attributes, that were created before that timestamp
             $logs = $DB->get_records_select("bcgt_logs", "timestamp < ?", array($ago));
-            
+
             mtrace("Found " . count($logs) . " logs to delete");
-            
-            foreach($logs as $log)
-            {
+
+            foreach ($logs as $log) {
                 $DB->delete_records("bcgt_logs", array("id" => $log->id));
                 $DB->delete_records("bcgt_log_attributes", array("logid" => $log->id));
             }
-                        
-        }
-        else
-        {
+
+        } else {
             mtrace("'keep_logs_for' set to never delete logs");
         }
-        
-        
+
         // Clean up tmp files
         mtrace("Cleaning up tmp files from [dataroot]/gradetracker");
         $result = \GT\GradeTracker::gc();
         mtrace("Deleted {$result} tmp files");
-                                
+
     }
-    
+
 }
--- classes/task/refresh_site_registration.php
+++ classes/task/refresh_site_registration.php
@@ -1,49 +1,51 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * This class defines the scheduled task to refresh site registration
- * 
- * @copyright 2015 Bedford College
- * @package Bedford College Grade Tracker
- * @version 1.0
- * @author Conn Warwicker <cwarwicker@bedford.ac.uk> <conn@cmrwarwicker.com> <moodlesupport@bedford.ac.uk>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
+ * @copyright   2011-2017 Bedford College, 2017 onwards Conn Warwicker
+ * @package     block_gradetracker
+ * @version     2.0
+ * @author      Conn Warwicker <conn@cmrwarwicker.com>
  */
 
 namespace block_gradetracker\task;
 
-require_once $CFG->dirroot . '/blocks/gradetracker/lib.php';
+defined('MOODLE_INTERNAL') or die();
 
-class refresh_site_registration extends \core\task\scheduled_task
-{
-        
+require_once($CFG->dirroot . '/blocks/gradetracker/lib.php');
+
+class refresh_site_registration extends \core\task\scheduled_task {
+
     /**
      * Get the name of the task
      * @return type
      */
-    public function get_name(){
+    public function get_name() {
         return get_string('task:refresh_site_registration', 'block_gradetracker');
     }
-    
+
     public function execute() {
-        
+
         mtrace("Attempting to update site registration");
-        
+
         $Site = new \GT\Site();
         $result = $Site->cron();
         mtrace("Result: " . $result);
-        
+
     }
-    
+
 }
